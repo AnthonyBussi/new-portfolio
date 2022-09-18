24bd6cb09a5634a877cdd2593b6398901407578d
Anthony Bussi <anthony.bussi@gmail.com>
2022-09-18 14:35:40 +0200

    test js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-let skillsDiv = document.querySelector('#skills');
-let homeDiv = document.querySelector('#home');
-let skillsDivPosition = skillsDiv.getBoundingClientRect();
-let homeDivPosition = homeDiv.getBoundingClientRect();
+// let skillsDiv = document.querySelector('#skills');
+// let homeDiv = document.querySelector('#home');
+// let skillsCtn = document.querySelector('.skills-container');
+// // let skillsDivPosition = skillsDiv.getBoundingClientRect();
+// let skillsDivPosition = skillsDiv.offsetTop;
+// // skillsDivPosition = 832
+// // let homeDivPosition = homeDiv.getBoundingClientRect();
+// let homeDivPosition = homeDiv.offsetTop;
 
-console.log(skillsDivPosition);
-console.log(homeDivPosition);
+// console.log(skillsDivPosition);
+// console.log(homeDivPosition);
 
-let y = window.scrollY;
-console.log(y);
+
+
+// window.addEventListener("scroll", () => {
+//     let y = window.scrollY;
+//     console.log(y);
+
+//     if(skillsDivPosition > y) {
+//         // skillsCtn.classList.remove("hidden");
+//         skillsCtn.classList.add("visible");
+
+//     }
+// })

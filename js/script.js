a5beefa13b8750ebc04ba74cfd80533be52e0bd5
Anthony Bussi <anthony.bussi@gmail.com>
2022-09-20 10:56:38 +0200

    Correction du script pour l'affichage des cercles sur mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-// let skillsDiv = document.querySelector('#skills');
-// let homeDiv = document.querySelector('#home');
-// let skillsCtn = document.querySelector('.skills-container');
+let homeDiv = document.querySelector('#home');
+let skillsDiv = document.querySelector('#skills');
+let projectsDiv = document.querySelector('#projects');
+let contactDiv = document.querySelector('#contact');
+
+//fonction pour afficher ou non les cercles rouge et bleu
+function displayCircle() {
+    let screenWidth = screen.width;
+    
+    if(screenWidth < 1024) {
+        skillsDiv.classList.remove("red-circle");
+        projectsDiv.classList.remove("blue-circle");
+        contactDiv.classList.remove("red-circle");
+        
+        console.log(screenWidth);
+    } else {
+        skillsDiv.classList.add("red-circle");
+        projectsDiv.classList.add("blue-circle");
+        contactDiv.classList.add("red-circle");        
+    }
+}
+
+
+window.addEventListener("resize", displayCircle);
+
 // // let skillsDivPosition = skillsDiv.getBoundingClientRect();
 // let skillsDivPosition = skillsDiv.offsetTop;
 // // skillsDivPosition = 832
@@ -22,18 +44,3 @@
 
 //     }
 // })
-
-let homeDiv = document.querySelector('#home');
-let skillsDiv = document.querySelector('#skills');
-let projectsDiv = document.querySelector('#projects');
-let contactDiv = document.querySelector('#contact');
-
-let screenWidth = screen.width;
-
-if(screenWidth < 1024) {
-    skillsDiv.classList.remove("red-circle");
-    projectsDiv.classList.remove("blue-circle");
-    contactDiv.classList.remove("red-circle");
-}
-
-console.log(screenWidth);
